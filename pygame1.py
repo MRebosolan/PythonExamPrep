6f06004cd6a4bfe7c249783146ba6d9f41b0095b
mrebosolan <superreba17@gmail.com>
2020-06-28 09:51:21 +0200

    days difference

--- pygame1.py
+++ pygame1.py
@@ -1,12 +1,45 @@
-import pygame as pg
+import math
 
-pg.init()
+first_date = input("Enter first date")
+second_date = input("Enter second date")
 
-screen = pg.display.set_mode((600,500))
-scrrect = screen.get_rect()
-pg.draw.circle(screen, (255,255,0), (scrrect.centerx, scrrect.centery), 250, 10)
-pg.display.flip()
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-pg.quit()
+def date_tuple(date_str):
+    lst = date_str.split("/")
+    for n, i in enumerate(lst):
+        lst[n] = int(i)
+    return lst
+
+date1 = date_tuple(first_date)
+date2 = date_tuple(second_date)
+
+date0 = [1, 1, 0]
+
+
+def difference(date, date0 = date0):
+    def leap_years(date):
+        counter = 0
+        for n in range(date[2]+1):
+            if (n % 4 == 0 and n % 100 != 0) or (n % 400 == 0):
+                counter += 1
+        return counter
+
+
+    days_from_years = date[2] * 365
+    days_from_months = sum(months[:(date[1] - 1)])
+    days_from_days = date[0]
+    total = days_from_years + days_from_months + days_from_days + leap_years(date)
+    return total
+
+print(abs(difference(date1) - difference(date2)))
+
+
+
+
+
+
+
+
 
 
